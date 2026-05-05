1ce49bd0ea4e33e71bb61c8d7e4a01e633a0c995
oliver11 <oliver11@iastate.edu>
2026-05-04 20:46:37 -0500

    added post to index.js

--- backend/index.js
+++ backend/index.js
@@ -34,3 +34,29 @@ app.get("/listFlights", async (req, res) => {
     res.status(200).send(results);
 }
 )
+app.post("/addUser", async (req, res) => {
+    try {
+        await client.connect();
+        const keys = Object.keys(req.body);
+        const values = Object.values(req.body);
+        
+        const newDocument = {
+            id: req.body.id,
+            name: req.body.name,
+            price: req.body.price,
+            description: req.body.description,
+            imageUrl: req.body.imageUrl,
+        };
+        console.log(newDocument);
+        const result = await db.collection("robot").insertOne(newDocument);
+        res.status(200);
+        res.send(result);
+    } catch (error) {
+        console.error("Could not add the new Robot" + error);
+        res.status(500);
+        res.send("Error adding new robot");
+    } finally {
+        await client.close();
+    }
+}
+);
