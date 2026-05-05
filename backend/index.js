da359631c9b92df05b9b706a6e78fce5e21cfe79
EdgarStrozzi <114312346+EdgarStrozzi@users.noreply.github.com>
2026-05-05 14:35:30 -0500

    Add Flights dataset for MongoDB seed

--- backend/index.js
+++ backend/index.js
@@ -7,21 +7,28 @@ app.use(cors());
 app.use(bodyParser.json());
 const port = "8080";
 const host = "localhost";
+
 //Add MongoDB
 const { MongoClient } = require("mongodb");
+
 //Add Constants
 const url = "mongodb://127.0.0.1:27017";
 const dbName = "aethera_airways";
 const client = new MongoClient(url);
 const db = client.db(dbName);
 const collection = "flights";
+
 app.listen(port, () => {
     console.log("App listening at http://%s:%s",
     host, port);
-});
+    }
+);
+
 app.get("/", async (req, res) => {
     res.send("welcom to aethera")
-})
+    }
+);
+
 app.get("/listFlights", async (req, res) => {
     await client.connect();
     const query ={};
@@ -32,8 +39,9 @@ app.get("/listFlights", async (req, res) => {
     .toArray();
     
     res.status(200).send(results);
-}
-)
+    }
+);
+
 app.post("/addUser", async (req, res) => {
     try {
         await client.connect();
@@ -58,5 +66,5 @@ app.post("/addUser", async (req, res) => {
     } finally {
         await client.close();
     }
-}
+    }
 );
